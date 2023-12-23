08c8d0933532580760247277317edb6c6908f448
suvraadeep <dassuvradeep9@gmail.com>
2023-12-23 21:21:28 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Course ChatBot',
+    version= '1.0.0',
+    author= 'Suvradeep',
+    author_email= 'dassuvradeep9@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
